b1a6c1efd4f6668d7ae8fd8e4a6c2c60734eef27
TLevion <levion96@gmail.com>
2025-11-27 20:02:48 +0500

    Updated seed.js

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://levion96_db_user:<password>@coffeeshopcluster.bxgwpwh.mongodb.net/coffee_shop_db?retryWrites=true&w=majority"
+    "Your Mango's Link"
   )
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.log("❌ Connection failed:", err.message));
